51b8dddf49e3f3d9c7af5a14d73f97c32de026fc
Alexander Shipulin <71063702+Sonechek@users.noreply.github.com>
2023-03-06 16:11:02 +0500

    Update main.py

--- lab_01/main.py
+++ lab_01/main.py
@@ -6,5 +6,9 @@ b = 1
 eps = math.pow(10, -6)
 
 
-functions.grafik(a, b)
-functions.kas(a, b, eps)
+# functions.grafik(a, b)
+# functions.kas(a, b, eps)
+# functions.gold(a, b, eps)
+# functions.newton(a, b, eps)
+print(functions.f(-37.2669908535841))
+print(functions.diffed(1))
