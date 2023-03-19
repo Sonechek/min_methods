331fd4ea6798117fe7b79e8a1606c37e761fc5f1
Sonechek <flex.tlex@gmail.com>
2023-03-19 20:49:52 +0500

    main

--- lab_02/functions.py
+++ lab_02/functions.py
@@ -1,11 +1,15 @@
 import math
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 def fprime(x1, x2):
-    return x1 ** 4 + x2 ** 4 + math.sqrt(2+ x1**2 + x2 ** 2)-2*x1+3*x2
+    return x1 ** 4 + x2 ** 4 + math.sqrt(2 + x1 ** 2 + x2 ** 2) - 2 * x1 + 3 * x2
+
 
 def fsx1(x1, x2):
-    return (4 * x1 ** 3) + (x1 / math.sqrt(x1** 2 + x2 ** 2 + 2)) - 2
+    return (4 * x1 ** 3) + (x1 / math.sqrt(x1 ** 2 + x2 ** 2 + 2)) - 2
 
 
 def fsx2(x1, x2):
@@ -13,7 +17,19 @@ def fsx2(x1, x2):
 
 
 def grad(x1, x2):
-    return [(4 * x1 ** 3) + (x1 / math.sqrt(x1** 2 + x2 ** 2 + 2)) - 2, (4 * x2 ** 3) + (x2 / math.sqrt(x1 ** 2 + x2 ** 2 + 2)) + 3]
+    return {(4 * x1 ** 3) + (x1 / math.sqrt(x1 ** 2 + x2 ** 2 + 2)) - 2,
+            (4 * x2 ** 3) + (x2 / math.sqrt(x1 ** 2 + x2 ** 2 + 2)) + 3}
+
+
+def grafik():
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+
+    x1, x2 = np.meshgrid(np.linspace(-0.2, 1, 1000), np.linspace(-1, 0, 1000))
+    z = x1 ** 4 + x2 ** 4 + np.sqrt(2 + x1 ** 2 + x2 ** 2) - 2 * x1 + 3 * x2
+
+    ax.plot_surface(x1, x2, z, color="purple")
+    plt.show()
 
 
 def gradient():
@@ -21,7 +37,6 @@ def gradient():
     alpha = 0.1
     alpha = alpha / n
     eps = 10 ** -6
-    X = [0, 0]
     x1 = 0
     x2 = 0
     acc = 0
@@ -35,6 +50,18 @@ def gradient():
         x1 = x11
         x2 = x21
 
-
-    print('x1, x2 - ' + str(x1), (x2))
+    print('x1, x2 - ' + str(x1), str(x2))
     print('Количество итераций - ' + str(acc))
+
+
+def hookjeevs():
+    x1 = 0
+    x2 = 0
+    di1 = 1
+    di2 = 1
+    alpha = 2
+    eps = 10 ** -6
+    acc = 0
+    while True:
+        x1 = fprime(x1 + di1, x2)
+        x2 = fprime(x1, x2 + di2)
--- lab_02/main.py
+++ lab_02/main.py
@@ -1,5 +1,5 @@
-import math
 import functions
 
 
 functions.gradient()
+functions.grafik()
